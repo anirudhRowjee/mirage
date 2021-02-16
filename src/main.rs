2c197b0f926f892783cd3a413cfc615a28df2e48
anirudhRowjee <ani.rowjee@gmail.com>
2021-02-17 02:02:45 +0530

    finished basic compiling! Support for first-order headings and plain text only.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
  */
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 
 /// Function to get the title of the application
 fn get_title() -> String
@@ -49,10 +49,10 @@ fn parse_markdown_file(filename: &str)
     // what part of structure of the markdown/html conversion we are in, and what we should do when
     // we encounter other closing tags, or, say, a newline character.
     // TODO think of a better way to do this - use a stack?
-    //
+
     // ptag : test if we are in paragraph
-    let mut ptag: bool = false;
     // htag: check if we are in heading
+    let mut ptag: bool = false;
     let mut htag: bool = false;
 
     // instantiate a vector to store the resultant HTML before we write it to a file.
@@ -77,8 +77,91 @@ fn parse_markdown_file(filename: &str)
         // }
         // ```
         let line_contents = line.unwrap();
-        println!("{}", &line_contents);
+        // println!("{}", &line_contents);
+
+        let mut output_line = String::new();
+        // we now have to parse the line character by character.
+        // we start by getting the first character
+        let mut first_char: Vec<char>  = line_contents.chars().take(1).collect();
+
+        // start deciding things
+        // since we only support H1 and P tags as of now, this will suffice
+        match first_char.pop()
+        {
+            Some('#') => {
+                // if it's open, close it - same for both tags
+                if ptag {
+                    ptag = false;
+                    output_line.push_str("</p>\n");
+                }
+                if htag {
+                    htag = false;
+                    output_line.push_str("</h1>\n");
+                }
+
+                // add a new heading, formally
+                htag = true;
+                output_line.push_str("<h1>");
+
+                // push every character apart from the hashtag onto the new string
+                // depends on the spec I guess, but in essence it should be one space between the
+                // hashtag and the start of the heading
+                output_line.push_str(&line_contents[1..]);
+            }
+            ,
+            _ => {
+                if !ptag {
+                    ptag = true;
+                    output_line.push_str("<p>");
+                }
+                output_line.push_str(&line_contents);
+            }
+        }
+
+        // even though most browsers would handle this, we need to cover all tags properly.
+        // check if there is an open paragraph tag 
+        if ptag {
+            ptag = false;
+            output_line.push_str("</p>\n");
+        }
+        // check if there is an open heading tag
+        if htag {
+            htag = false;
+            output_line.push_str("</h1>\n");
+        }
+
+        // if the line was empty, it would be considered a blank p tag
+        // making sure we don't push blank paragraph tags, we just push the contents of this line
+        // to the output string (tokens)
+        if output_line != "<p></p>\n" 
+        {
+            tokens.push(output_line);
+        }
     }
+
+    /*
+    println!("DEBUG >>> ");
+    for line in &tokens {
+        println!("{}", line);
+    }
+    */
+
+
+    // derive the name of the HTML file from the markdown file
+    let filename_length = filename.len() - 3;
+    let mut new_filename = String::new();
+    // make a new string with the name of the file, plus ".html"
+    new_filename.push_str(&filename[..filename_length]);
+    new_filename.push_str(".html");
+
+    // open the new HTML file for writing 
+    let mut output_file = File::create(&new_filename).expect(" { ERROR } Could not open file to write! ");
+
+    for line in &tokens {
+        output_file.write_all(line.as_bytes()).expect(" { ERROR } Could not write to file! ")
+    }
+
+    println!("[INFO] Parsing complete! Output written to {}", &new_filename);
 
 }
 
